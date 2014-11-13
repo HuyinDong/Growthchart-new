25a724b9734022b8f0f2df203051104c061e917e
dong <dejorkoffe@gmail.com>
2014-11-13 13:18:33 -0400

    ajax2

--- d3test/BMI.php
+++ d3test/BMI.php
@@ -1,11 +1,6 @@
 <?php
 
-   $sex =  $_GET['sex'];
-    $ktype =  $_GET['ktype'];
-   $age = $_GET['age'];
-   $weight = $_GET['weight'];
-   $stature = $_GET['stature'];
-   $hc = $_GET['hair'];
+
    $str = file_get_contents('bmi.json');
    $json = json_decode($str, true); 
    echo $json['bmi_per'];
