6d57e4ca7bd270ac8f48cc8e33927ac4ca40705f
Huyin Dong <dejorkoffe@gmail.com>
2015-09-12 00:50:59 -0400

    add auth

--- config/express.js
+++ config/express.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var index = require('../app/routes/index.server.router');
 var data = require('../app/routes/data.server.router');
 var login = require('../app/routes/login.server.router');
+var auth = require("http-auth");
 module.exports = function(){
     var app = express();
 
@@ -15,7 +16,12 @@ module.exports = function(){
     app.set('view engine', 'ejs');
 
     app.use(express.static('./public'));
-
+    app.use(express.static('./public'));
+    var basic = auth.basic({
+        realm: "Private area",
+        file: "htpasswd"
+    });
+    app.use(auth.connect(basic));
     index(app);
     data(app);
     login(app);
